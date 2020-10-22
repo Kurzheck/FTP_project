a39690a333d0b72f8520e79ce37f7b766ccec26c
isaac chou <isaaxou@icloud.com>
2020-10-22 08:49:37 +0000

    nothing

--- tcp/server/src/global.h
+++ tcp/server/src/global.h
@@ -2,7 +2,6 @@
 #define PATH_LENGTH 256
 #define RESPONSE_LENGTH 256
 int listenfd;
-//int connectionfd;
 int listenPort = 21;
 char rootPath[PATH_LENGTH] = "/tmp";
 char serverIP[30] = "127.0.0.1";
