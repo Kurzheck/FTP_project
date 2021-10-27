94ad918856b9c7fd43ead849852df2d9877a0f4a
Isaac Chou <isaaxou@icloud.com>
2021-10-27 21:33:54 +0800

    modify ui, add init functions

--- tcp/client/common.py
+++ tcp/client/common.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import sys
 
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QProgressBar, QPushButton, QRadioButton, QSpinBox, QTableWidget, QTextEdit, QWidget, QLineEdit
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
@@ -11,6 +11,10 @@ class ClientWindow(QWidget):
     def __init__(self):
         super(ClientWindow, self).__init__()
         self.load_ui()
+        self.setFixedSize(700, 800)
+        self.init_widget()
+        self.init_status()
+        self.init_connect()
 
     def load_ui(self):
         loader = QUiLoader()
@@ -19,3 +23,32 @@ class ClientWindow(QWidget):
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file, self)
         ui_file.close()
+
+    def init_widget(self):
+        self.lineEdit_IP = self.findChild(QLineEdit, "lineEdit_IP")
+        self.spinBox_port = self.findChild(QSpinBox, "spinBox_port")
+        self.lineEdit_username = self.findChild(QLineEdit, "lineEdit_username")
+        self.lineEdit_password = self.findChild(QLineEdit, "lineEdit_password")
+        self.radioButton_PASV = self.findChild(QRadioButton, "radioButton_PASV")
+        self.radioButton_PORT = self.findChild(QRadioButton, "radioButton_PORT")
+        self.pushButton_login = self.findChild(QPushButton, "pushButton_login")
+        self.pushButton_logout = self.findChild(QPushButton, "pushButton_logout")
+        self.lineEdit_cwd = self.findChild(QLineEdit, "lineEdit_cwd")
+        self.pushButton_go = self.findChild(QPushButton, "pushButton_go")
+        self.tableWidget_ls = self.findChild(QTableWidget, "tableWidget_ls")
+        self.pushButton_mkdir = self.findChild(QPushButton, "pushButton_mkdir")
+        self.pushButton_rm = self.findChild(QPushButton, "pushButton_rm")
+        self.pushButton_mkdir = self.findChild(QPushButton, "pushButton_mkdir")
+        self.pushButton_upload = self.findChild(QPushButton, "pushButton_upload")
+        self.pushButton_download = self.findChild(QPushButton, "pushButton_download")
+        self.progressBar = self.findChild(QProgressBar, "progressBar")
+        self.textEdit_log = self.findChild(QTextEdit, "textEdit_log")
+
+    def init_status(self):
+        self.lineEdit_username.setText("anonymous")
+
+    def init_connect(self):
+        self.pushButton_login.clicked.connect(self.login());
+
+    def login(self):
+        pass
